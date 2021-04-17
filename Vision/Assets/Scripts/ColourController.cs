7150103b8cc4802a39d8179d0764d94de040521c
Naomi Kaufman <accounts@warducks.com>
2021-04-17 18:41:41 +0100

    Final Tweaks

---
+++ Vision/Assets/Scripts/ColourController.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class ColourController : MonoBehaviour
+{
+    [SerializeField] private GameObject RedCam;
+    [SerializeField] private GameObject BlueCam;
+    [SerializeField] private GameObject YellowCam;
+    [SerializeField] private GameObject RedMap;
+    [SerializeField] private GameObject BlueMap;
+    [SerializeField] private GameObject YellowMap;
+    [SerializeField] private int RedLimit = 5;
+    [SerializeField] private int BlueLimit = 5;
+    [SerializeField] private int YellowLimit = 5;
+
+
+    void Start()
+    {
+        RedMap.SetActive(false);
+        BlueMap.SetActive(false);
+        YellowMap.SetActive(false);    
+
+        RedCam.SetActive(false);
+        BlueCam.SetActive(false);
+        YellowCam.SetActive(false);
+    }
+
+public void Red(InputAction.CallbackContext context)
+{
+    if (RedLimit > 0)
+    {
+        RedLimit -= 1;
+
+        RedCam.SetActive(true);
+            BlueCam.SetActive(false);
+            YellowCam.SetActive(false);
+
+
+            RedMap.SetActive(true);
+        BlueMap.SetActive(false);
+            YellowMap.SetActive(false);
+
+    }
+}
+
+
+public void Blue(InputAction.CallbackContext context)
+{
+    if (BlueLimit > 0)
+    {
+        BlueLimit -= 1;
+
+        RedCam.SetActive(false);
+            BlueCam.SetActive(true);
+            YellowCam.SetActive(false);
+
+
+            RedMap.SetActive(false);
+        BlueMap.SetActive(true);
+        YellowMap.SetActive(false);
+
+
+    }
+}
+
+
+public void Yellow(InputAction.CallbackContext context)
+{
+    if (YellowLimit > 0)
+    {
+        YellowLimit -= 1;
+
+        RedCam.SetActive(false);
+            BlueCam.SetActive(false);
+            YellowCam.SetActive(true);
+
+
+            RedMap.SetActive(false);
+        BlueMap.SetActive(false);
+        YellowMap.SetActive(true);
+
+
+    }
+}
+        
+
+}
